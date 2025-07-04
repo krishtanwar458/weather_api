6d78ea0d6b95a604b940fb420f74520697c99e75
Krish Tanwar <krishtanwar458@gmail.com>
2025-07-04 12:56:04 +0530

    added about and home page rerouting. Added a weather API home page. Added an initial URL based on station and date to fetch temperature data

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
+import pandas as pd
 
-app = Flask('Website')
+app = Flask(__name__)
 
-@app.route('/home')
+
+@app.route('/')
 def home():
-    return render_template('tutorial.html')
+    return render_template('home.html')
 
-@app.route('/about/')
-def about():
-    return render_template('about.html')
+@app.route('/API/v1/<station>/<date>')
+def about(station, date):
+    df = pd.read_csv('data.csv')
+    temprature = df.station(date)
+    return {'station': station, 
+            'date': date, 
+            'temprature': temprature}
 
-app.run(debug=True, use_reloader=False)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)
